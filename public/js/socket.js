ee7e38592567889865895fa40a6ebd48a06c86cc
Mark Rodighiero <markatango@gmail.com>
2024-04-13 17:37:24 -0700

    moved socket client code to public folder

---
+++ public/js/socket.js
@@ -0,0 +1,14 @@
+var socket = io();
+
+socket.emit('joining msg', name);
+
+$('form').submit(function(e) {
+    e.preventDefault();            // will prevent page reloading
+    socket.emit('chat message', (name + ':  ' + $('#m').val()));
+            $('#messages').append($('<li id="list">').text('You:  ' + $('#m').val()));
+    $('#m').val('');
+    return false;
+});
+socket.on('chat message', function(msg){
+    $('#messages').append($('<li>').text(msg));
+});
